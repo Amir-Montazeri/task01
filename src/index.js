d99b39a481bd9f3200ab35f67cc7f703e4852640
Amir Mahdi Montazeri <98890089+Amir-Montazeri@users.noreply.github.com>
2022-02-18 13:39:12 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ ReactDOM.render(
     <ThemeProvider theme={themes.default}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
-  , document.querySelector('#root'));
+  </React.StrictMode>,
+  document.querySelector('#root'),
+);
